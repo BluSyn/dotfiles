de219aadc23bbc2418dc89dde986f180ebe2e78e
BluSyn <blusyn@protonmail.com>
2021-09-05 03:04:57 +0000

    +fish shell config (testing)

---
+++ config.fish
@@ -0,0 +1,71 @@
+# GIT Shortcuts
+abbr -a g 'git'
+abbr -a gs 'git status'
+abbr -a gc 'TZ=UTC0 git commit'
+abbr -a ga 'git add'
+abbr -a gd 'git diff'
+abbr -a gps 'git push'
+abbr -a gpl 'git pull'
+abbr -a gr 'git remote'
+abbr -a grb 'git rebase'
+abbr -a gl 'git log'
+abbr -a gco 'git checkout'
+abbr -a gb 'git branch'
+abbr -a gm 'git merge'
+abbr -a gt 'git tag'
+
+# Docker
+abbr -a d 'docker'
+abbr -a dps 'docker ps'
+abbr -a dl 'docker logs -f --tail=1000'
+abbr -a dc 'docker-compose'
+abbr -a dockerclean 'docker rmi \$(docker images | grep "<none>" | awk "{print \$3}")'
+abbr -a dockerrm 'docker rm \$(docker ps -aq)'
+
+
+# exa > ls
+if command -v exa > /dev/null
+	abbr -a l 'exa'
+	abbr -a ll 'exa -labgF --git --time-style long-iso'
+    abbr -a lls 'exa -labgF --git --git-ignore --time-style long-iso --ignore-glob=".git" --tree -L 2'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+end
+
+# Auto attach existing tmux window on SSH
+function msh
+    mosh --no-init --predict=neber -- "$argv" sh -c "TERM=xterm-256color tmux attach || tmux new"
+end
+
+function ssh
+    /usr/local/bin/ssh -t "$argv" "TERM=xterm-256color tmux attach || tmux new"
+end
+
+function sshr
+    /usr/local/bin/ssh "$argv"
+end
+
+# Fish git prompt
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream 'none'
+
+function fish_prompt
+    # Line 1: <dir> [<git>] \n
+	set_color C0C0C0; echo -n (prompt_pwd)
+    set_color green; printf '%s ' (__fish_git_prompt)
+    echo ""
+
+    # Line 2: [<time>][<user>@<host>] $
+    set_color normal; echo -n "["
+    set_color C0C0C0; echo -n (date "+%H:%M:%S")
+    set_color normal; echo -n "]["
+    set_color yellow; echo -n (whoami)
+    set_color C0C0C0; echo -n "@"
+    set_color blue; echo -n (hostname -s)
+    set_color normal; echo -n "] \$ "
+end
+
