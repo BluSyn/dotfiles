8912767c924ac4cfd536b096bfb8eb22ad25b1a3
BluSyn <blusyn@protonmail.com>
2022-10-29 08:26:47 +0000

    install: cleanup

--- install.sh
+++ install.sh
@@ -2,28 +2,14 @@
 
 # Assumes .dotfiles is in ~
 
-# BASH
 ln -si ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -si ~/.dotfiles/bash/profile ~/.profile
 ln -si ~/.dotfiles/bash/bashTweaks ~/.bashTweaks
 
-# FISH
-ln -si ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
-
-# GIT
-ln -si ~/.dotfiles/git ~/.config/git
-
-# TMUX
 ln -si ~/.dotfiles/tmux ~/config/tmux
-
-# NeoVIM
 ln -si ~/.dotfiles/nvim ~/.config/nvim
-
-# Helix
 ln -si ~/.dotfiles/helix ~/.config/helix
-
-# Allacritty
 ln -si ~/.dotfiles/alacritty ~/.config/alacritty
-
-# Other
 ln -si ~/.dotfiles/htop/htoprc ~/.config/htop/htoprc
+
+ln -si ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
