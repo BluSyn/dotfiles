e04f1aff02c3a4e398f3005d8231a617bf2c386d
BluSyn <blusyn@protonmail.com>
2022-10-29 12:58:05 +0000

    rename .dotfiles -> dotfiles

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Assumes .dotfiles is in ~
+# Assumes dotfiles is in ~
 
-ln -si ~/.dotfiles/bash/bashrc ~/.bashrc
-ln -si ~/.dotfiles/bash/profile ~/.profile
-ln -si ~/.dotfiles/bash/bashTweaks ~/.bashTweaks
+ln -si ~/dotfiles/bash/bashrc ~/.bashrc
+ln -si ~/dotfiles/bash/profile ~/.profile
+ln -si ~/dotfiles/bash/bashTweaks ~/.bashTweaks
 
-ln -si ~/.dotfiles/git ~/.config/git
-ln -si ~/.dotfiles/tmux ~/.config/tmux
-ln -si ~/.dotfiles/tmux ~/.config/tmuxp
-ln -si ~/.dotfiles/nvim ~/.config/nvim
-ln -si ~/.dotfiles/helix ~/.config/helix
-ln -si ~/.dotfiles/alacritty ~/.config/alacritty
-ln -si ~/.dotfiles/htop/htoprc ~/.config/htop/htoprc
+ln -si ~/dotfiles/git ~/.config/git
+ln -si ~/dotfiles/tmux ~/.config/tmux
+ln -si ~/dotfiles/tmuxp ~/.config/tmuxp
+ln -si ~/dotfiles/nvim ~/.config/nvim
+ln -si ~/dotfiles/helix ~/.config/helix
+ln -si ~/dotfiles/alacritty ~/.config/alacritty
+ln -si ~/dotfiles/htop/htoprc ~/.config/htop/htoprc
 
-ln -si ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -si ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
