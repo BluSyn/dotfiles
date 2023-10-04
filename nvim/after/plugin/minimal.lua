2515e60df4ef71c8cc912b5e6df6814a758b7c91
Steven Bower <steven@bower.club>
2023-10-04 23:51:38 +0000

    nvim: fix colors

--- nvim/after/plugin/minimal.lua
+++ nvim/after/plugin/minimal.lua
@@ -15,13 +15,19 @@ end
 highlight("Exception", { fg = colors.pink })
 highlight("MatchParen", { fg = colors.orange_wr, style = 'underline' })
 highlight("@field", { fg = colors.white1 })
+highlight("@include", { fg = colors.pink })
 highlight("@property", { fg = colors.white1 })
 highlight("@variable", { fg = colors.red_key_w })
 highlight("@parameter", { fg = colors.red_key_w })
+highlight("@constructor", { fg = colors.blue_func })
 highlight("@lsp.type.field", { fg = colors.white1 })
 highlight("@lsp.type.variable", { fg = colors.red_key_w })
 highlight("@lsp.type.parameter", { fg = colors.red_key_w })
 highlight("@lsp.type.property", { fg = colors.white1 })
 highlight("@lsp.type.interface", { fg = colors.yellow })
 highlight("@lsp.type.class", { fg = colors.yellow })
+
+-- Typescript
+highlight("@type.builtin.typescript", { fg = colors.white1 })
+highlight("@punctuation.bracket.typescript", { fg = colors.pink })
 
