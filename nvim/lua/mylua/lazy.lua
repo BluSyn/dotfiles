2105d8a411158254efc5c373b4e8161489e24871
Steven Bower <steven@bower.club>
2023-10-04 23:13:09 +0000

    nvim: telescope show hidden files

--- nvim/lua/mylua/lazy.lua
+++ nvim/lua/mylua/lazy.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        dependencies  = { {'nvim-lua/plenary.nvim'} }
+        dependencies  = { {'nvim-lua/plenary.nvim'} },
     },
 
     {
