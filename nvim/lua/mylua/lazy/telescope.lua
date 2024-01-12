0853171b9f239502d73e9a34f6e32fdab5dcb63c
Steven Bower <steven@bower.club>
2024-01-12 05:21:36 +0000

    nvim: telescope version

--- nvim/lua/mylua/lazy/telescope.lua
+++ nvim/lua/mylua/lazy/telescope.lua
@@ -1,11 +1,11 @@
 return {
     'nvim-telescope/telescope.nvim',
 
-    tag          = '0.1.4',
+    tag          = '0.1.5',
     dependencies = {
         'nvim-lua/plenary.nvim',
     },
-    config = function()
+    config       = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>/', function()
             require('telescope.builtin').find_files({
