200f05c3c14a8e1ba7e8aef7745ee3fd08cc5a6a
Steven Bower <steven@bower.club>
2024-05-25 05:34:51 +0000

    nvim: harpoon keys

--- nvim/lua/mylua/lazy/harpoon.lua
+++ nvim/lua/mylua/lazy/harpoon.lua
@@ -9,8 +9,8 @@ return {
         vim.keymap.set("n", "<leader>\\", function() harpoon:list():append() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set('n', 'g]', function() harpoon:list():next() end)
-        vim.keymap.set('n', 'g[', function() harpoon:list():prev() end)
+        vim.keymap.set('n', 'gn', function() harpoon:list():next() end)
+        vim.keymap.set('n', 'gp', function() harpoon:list():prev() end)
 
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
