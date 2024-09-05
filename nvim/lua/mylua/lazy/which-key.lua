539bab889015d713c2b9589d622868e64701756a
Steven Bower <steven@bower.club>
2024-09-05 05:02:30 +0000

    nvim: disable which-key due to load errors

--- nvim/lua/mylua/lazy/which-key.lua
+++ nvim/lua/mylua/lazy/which-key.lua
@@ -1,7 +1,7 @@
 return {
-    'folke/which-key.nvim',
-    config = function()
-        local wk = require('which-key')
-        wk.register()
-    end
+    -- 'folke/which-key.nvim',
+    -- config = function()
+    --     local wk = require('which-key')
+    --     wk.register()
+    -- end
 }
