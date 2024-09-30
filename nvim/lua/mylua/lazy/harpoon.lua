9badb48bce5f6bc986d4ec43c4740c07a6a3e607
Steven Bower <steven@bower.club>
2024-09-30 01:17:54 +0000

    nvim: harpoon

--- nvim/lua/mylua/lazy/harpoon.lua
+++ nvim/lua/mylua/lazy/harpoon.lua
@@ -6,7 +6,7 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>\\", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>\\", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set('n', 'gn', function() harpoon:list():next() end)
