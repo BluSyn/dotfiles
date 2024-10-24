2e915cac32443bbdc5b857b86193ccfffc3d9e5a
Steven Bower <steven@bower.club>
2024-10-24 05:24:53 +0000

    nvim: allow easy creation of files in dir view

--- nvim/lua/mylua/init.lua
+++ nvim/lua/mylua/init.lua
@@ -16,3 +16,24 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('mylua.lazy')
+
+-- Create new file in netrw with 'a'
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'netrw',
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, 'n', 'a', ':lua CreateFile()<CR>', { noremap = true, silent = true })
+    end,
+})
+
+function CreateFile()
+    -- Get the current directory shown in netrw (current buffer dir)
+    local current_dir = vim.fn.expand('%:p:h')
+
+    -- Get the full path to the new file
+    local new_file = vim.fn.input('New file: ', current_dir .. '/')
+
+    if new_file ~= "" then
+        vim.cmd('e ' .. new_file)
+        vim.cmd('w')
+    end
+end
