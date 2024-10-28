623c3e07829914663b996d7b5a12be6242776a8b
Steven Bower <steven@bower.club>
2024-10-28 04:38:21 +0000

    nvim: add mini.splitjoin

--- nvim/lua/mylua/lazy/mini.lua
+++ nvim/lua/mylua/lazy/mini.lua
@@ -4,5 +4,12 @@ return {
     config = function()
         require('mini.pairs').setup()
         require('mini.surround').setup()
+        require('mini.splitjoin').setup(
+            {
+                mappings = {
+                    toggle = '<leader>s',
+                }
+            }
+        )
     end
 }
