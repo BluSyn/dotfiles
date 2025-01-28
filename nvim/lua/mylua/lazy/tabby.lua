649623c57dc34def6f0c06d252c728823d0aaa8c
Steven Bower <steven@bower.club>
2025-01-28 02:56:56 +0000

    nvim: ai assistant experiments

--- nvim/lua/mylua/lazy/tabby.lua
+++ nvim/lua/mylua/lazy/tabby.lua
@@ -6,7 +6,7 @@ return {}
 --         'neovim/nvim-lspconfig',
 --     },
 --     init = function()
---         vim.g.tabby_keybinding_accept = '<C-l>'
+--         vim.g.tabby_inline_completion_keybinding_accept = '<C-l>'
 --         vim.g.tabby_agent_start_command = { 'npx', 'tabby-agent', '--stdio' }
 --         vim.g.tabby_inline_completion_trigger = 'auto'
 --     end,
