9d4a4727d898bec62262b135cf9f8565905f4845
Steven Bower <steven@bower.club>
2025-02-17 02:17:29 +0000

    nvim: create nested dirs in file view

--- nvim/lua/mylua/init.lua
+++ nvim/lua/mylua/init.lua
@@ -17,23 +17,31 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('mylua.lazy')
 
--- Create new file in netrw with 'a'
+-- Create new file or directory in netrw with 'a'
 vim.api.nvim_create_autocmd('FileType', {
     pattern = 'netrw',
     callback = function()
-        vim.api.nvim_buf_set_keymap(0, 'n', 'a', ':lua CreateFile()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(0, 'n', 'a', ':lua CreateFileOrDir()<CR>', { noremap = true, silent = true })
     end,
 })
 
-function CreateFile()
+function CreateFileOrDir()
     -- Get the current directory shown in netrw (current buffer dir)
-    local current_dir = vim.fn.expand('%:p:h')
+    local current_dir = vim.b.netrw_curdir
 
-    -- Get the full path to the new file
-    local new_file = vim.fn.input('New file: ', current_dir .. '/')
+    -- Get the full path to the new file or directory
+    local new_path = vim.fn.input('New file or directory: ', current_dir .. '/')
 
-    if new_file ~= "" then
-        vim.cmd('e ' .. new_file)
-        vim.cmd('w')
+    if new_path ~= "" then
+        if new_path:match('.*/$') then
+            -- Create directory if path ends with a slash
+            vim.fn.mkdir(new_path, 'p')
+        else
+            -- Create nested directories if needed and then create the file
+            local dir = vim.fn.fnamemodify(new_path, ':h')
+            vim.fn.mkdir(dir, 'p')
+            vim.cmd('e ' .. new_path)
+            vim.cmd('w')
+        end
     end
 end
