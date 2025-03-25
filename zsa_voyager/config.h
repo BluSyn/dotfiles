bea9d74a77497dadff0cf101409b68129a2705d7
Steven Bower <steven@bower.club>
2025-03-25 04:13:31 +0000

    zsa: latest keymaps

--- zsa_voyager/config.h
+++ zsa_voyager/config.h
@@ -1,8 +1,3 @@
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
-*/
-
 #undef TAPPING_TERM
 #define TAPPING_TERM 150
 
@@ -10,7 +5,7 @@
 #define RGB_MATRIX_TIMEOUT 900000
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "6Rm9X/EXGzY"
+#define SERIAL_NUMBER "6Rm9X/dPZYbZ"
 #define LAYER_STATE_8BIT
 #define HCS(report) host_consumer_send(record->event.pressed ? report : 0); return false
 
--- zsa_voyager/rules.mk
+++ zsa_voyager/rules.mk
@@ -1,5 +1,3 @@
-# Set any rules.mk overrides for your specific keymap here.
-# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
@@ -7,3 +5,4 @@ ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
 SPACE_CADET_ENABLE = no
 CAPS_WORD_ENABLE = yes
+LAYER_LOCK_ENABLE = yes
