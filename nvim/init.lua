9facfa84d3563695c31a62c45cd12e24797267c5
Steven Bower <steven@bower.club>
2025-06-08 13:55:00 +0000

    aider: auto reload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,4 @@
 -- Based on: https://github.com/ThePrimeagen/init.lua
 require('mylua')
+
+vim.opt.autoread = true
--- nvim/lua/mylua/lazy/aider.lua
+++ nvim/lua/mylua/lazy/aider.lua
@@ -1,18 +1,23 @@
 return {
-    "GeorgesAlkhouri/nvim-aider",
-    cmd = "Aider",
+    'GeorgesAlkhouri/nvim-aider',
+    cmd = 'Aider',
     -- Example key mappings for common actions:
     keys = {
-        { "<leader>a/", "<cmd>Aider toggle<cr>",       desc = "Toggle Aider" },
-        { "<leader>as", "<cmd>Aider send<cr>",         desc = "Send to Aider", mode = { "n", "v" } },
-        { "<leader>ac", "<cmd>Aider command<cr>",      desc = "Aider Commands" },
-        { "<leader>ab", "<cmd>Aider buffer<cr>",       desc = "Send Buffer" },
-        { "<leader>a+", "<cmd>Aider add<cr>",          desc = "Add File" },
-        { "<leader>a-", "<cmd>Aider drop<cr>",         desc = "Drop File" },
-        { "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
-        { "<leader>aR", "<cmd>Aider reset<cr>",        desc = "Reset Session" },
+        { '<leader>a/', '<cmd>Aider toggle<cr>',       desc = 'Toggle Aider' },
+        { '<leader>as', '<cmd>Aider send<cr>',         desc = 'Send to Aider', mode = { 'n', 'v' } },
+        { '<leader>ac', '<cmd>Aider command<cr>',      desc = 'Aider Commands' },
+        { '<leader>ab', '<cmd>Aider buffer<cr>',       desc = 'Send Buffer' },
+        { '<leader>a+', '<cmd>Aider add<cr>',          desc = 'Add File' },
+        { '<leader>a-', '<cmd>Aider drop<cr>',         desc = 'Drop File' },
+        { '<leader>ar', '<cmd>Aider add readonly<cr>', desc = 'Add Read-Only' },
+        { '<leader>aR', '<cmd>Aider reset<cr>',        desc = 'Reset Session' },
     },
     dependencies = {
-        "folke/snacks.nvim",
+        'folke/snacks.nvim',
     },
+    config = function()
+        require('nvim_aider').setup({
+            auto_reload = true,
+        })
+    end,
 }
