c14f05f37822572e5bfade6f60a28f4422bc3176
Steven Bower <steven@bower.club>
2025-10-06 13:29:26 +0000

    nvim: fix snacks settings

--- nvim/lua/mylua/lazy/snacks.lua
+++ nvim/lua/mylua/lazy/snacks.lua
@@ -1,23 +1,21 @@
 return {
-    "folke/snacks.nvim",
+    'folke/snacks.nvim',
     priority = 1000,
     lazy = false,
     ---@type snacks.Config
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
         bigfile = { enabled = true },
         dashboard = { enabled = true },
         explorer = { enabled = true },
-        indent = { enabled = true },
         input = { enabled = true },
         picker = { enabled = true },
         notifier = { enabled = true },
         quickfile = { enabled = true },
-        scope = { enabled = true },
-        scroll = { enabled = true },
-        statuscolumn = { enabled = true },
         words = { enabled = true },
+        terminal = { enabled = true },
+        gitbrowse = { enabled = true }
+    },
+    keys = {
+        { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Git Browse', mode = { 'n', 'v' } },
     },
 }
